aace2b90e78dbea113769ccd83c5941ead350b93
gureann <lourh@shanghaitech.edu.cn>
2020-11-10 20:03:07 +0800

    Add params to control the previously fixed functions in count_bar

--- bioplotkit/plots/count_bar.py
+++ bioplotkit/plots/count_bar.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 from bioplotkit.drawing_area import set_thousand_separate
 
 
-def count_bar(data,
+def count_bar(data, sort_index=True,
               ylim=None, xlim=None,
               bar_width=0.6, bar_color=None,
+              anno_format=',', anno_rotation=90,
+              x_ticks=None, x_tick_rotation=0,
+              y_tick_thousand=True,
               xlabel='Missed cleavage', ylabel='Number of peptides',
               title='',
               ax=None):
@@ -21,15 +24,25 @@ def count_bar(data,
         ser = data
     else:
         raise TypeError
-    ser = ser.sort_index()
+    if sort_index:
+        ser = ser.sort_index()
 
-    list_data = list(ser.items())
+    data_keys, data_values = list(zip(*pd.Series(ser).items()))
+    if x_ticks is not None:
+        bar_xsite = x_ticks
+    else:
+        bar_xsite = list(range(1, len(data_keys) + 1))
 
-    ax.bar(*list(zip(*list_data)), width=bar_width, color=bar_color)
-    for idx, num in list_data:
-        ax.annotate(format(num, ','), (idx, num / 2), ha='center')
+    ax.bar(bar_xsite, data_values, width=bar_width, color=bar_color)
+    for site, value in zip(bar_xsite, data_values):
+        ax.annotate(format(value, anno_format), (site, value / 2),
+                    ha='center', rotation=anno_rotation)
 
-    set_thousand_separate(ax, 'y')
+    ax.set_xticks(bar_xsite)
+    ax.set_xticklabels(data_keys, rotation=x_tick_rotation)
+
+    if y_tick_thousand:
+        set_thousand_separate(ax, 'y')
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_title(title)
